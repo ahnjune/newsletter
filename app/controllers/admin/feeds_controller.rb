622927c445e3b9e576c7539353825f71e22cf4a0
Collin VanDyck <gluedtomyseat@gmail.com>
2009-01-22 16:20:55 -0500

    Dry up view work, some more navigation elements

--- app/controllers/admin/feeds_controller.rb
+++ app/controllers/admin/feeds_controller.rb
@@ -1,7 +1,24 @@
 class Admin::FeedsController < ApplicationController
+
+  before_filter :load_feed
   
   def index
     @feeds = Feed.ordered
+  end
+  
+  def create
+    @feed = Feed.new(params[:feed])
+    if @feed.save
+      flash[:notice] = "Feed created"
+      redirect_to admin_feeds_path and return
+    end
+    render :action => "new"
+  end
+
+  private
+  
+  def load_feed
+    @feed = Feed.find(params[:id]) if params[:id]
   end
   
 end
--- app/models/feed.rb
+++ app/models/feed.rb
@@ -3,6 +3,9 @@ class Feed < ActiveRecord::Base
   validates_presence_of :name
   validates_presence_of :url
   
+  validates_uniqueness_of :name
+  validates_uniqueness_of :url
+  
   named_scope :ordered, :order => "name"
   
 end
