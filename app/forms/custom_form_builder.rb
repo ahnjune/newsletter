375d0bbc1f19b4577cf7ca31d0989b8305dfe5e6
Collin VanDyck <gluedtomyseat@gmail.com>
2009-01-23 11:22:19 -0500

    Fixed some of the form styling, better support in form builder

--- app/forms/custom_form_builder.rb
+++ app/forms/custom_form_builder.rb
@@ -2,11 +2,14 @@ class CustomFormBuilder < ActionView::Helpers::FormBuilder
   
   %w[text_field text_area password_field].each do |method_name|
     define_method method_name do |field_name, *args|
-      content = [label(field_name), super].join("<br/>")
-      @template.content_tag(:p, content, :class => method_name)
+      options = args.extract_options!
+      label = label(field_name)
+      field = super(field_name, *[args, options.merge(:class => method_name)].flatten)
+      @template.content_tag(:p, [label, field].join("<br/"))
     end
   end
   
+  private
   
   
 end
