eb16f3cbb2e2c57d432399c1e3f5d042c39cbd16
Collin VanDyck <gluedtomyseat@gmail.com>
2009-01-25 17:30:27 -0500

    Feed items now build properly

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,14 +1,18 @@
 class Feed < ActiveRecord::Base
   
-  validates_presence_of :title
-  validates_presence_of :url
+  has_many :feed_items, :dependent => :destroy
   
-  validates_uniqueness_of :title
+  validates_presence_of :url  
   validates_uniqueness_of :url
   
   named_scope :ordered, :order => "title"
   
   before_validation :fetch
+  after_update :save_feed_items
+  
+  def title
+    super || url
+  end
   
   def to_param
     [id, title.downcase.gsub(/\W/,"")].join("-")
@@ -17,7 +21,28 @@ class Feed < ActiveRecord::Base
   def fetch
     returning FeedTools::Feed.open(url) do |fetched|
       self.title = fetched.title
+      fetched.items.each do |item|
+        add_fetched_item(item)
+      end
     end
+  end
+  
+  def add_fetched_item(item)
+    logger.info "Item: #{item.title}"
+    if feed_item = feed_items.detect { |feed_item| feed_item.link == item.link}
+      # update the feed item
+    else
+      # add the feed item
+      feed_items.push FeedItem.new({
+        :feed => self,
+        :title => item.title,
+        :link => item.link
+      })
+    end
+  end
+  
+  def save_feed_items
+    feed_items.each { |feed_item| feed_item.save(false) }
   end
   
 end
--- app/models/feed_item.rb
+++ app/models/feed_item.rb
@@ -1,2 +1,6 @@
 class FeedItem < ActiveRecord::Base
+  
+  belongs_to :feed
+  validates_presence_of :feed
+
 end
