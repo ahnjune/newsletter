8b6c49379a2d73680c6915bcf4ab8223f1470f1d
Collin VanDyck <gluedtomyseat@gmail.com>
2009-01-26 13:45:57 -0500

    Users taken to the invite screen after signing up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,8 +15,8 @@ class UsersController < ApplicationController
       # button. Uncomment if you understand the tradeoffs.
       # reset session
       self.current_user = @user # !! now logged in
-      redirect_back_or_default('/')
-      flash[:notice] = "Thanks for signing up!"
+      flash[:notice] = "Thanks for signing up.  Invite other friends to Thinkful."
+      redirect_to new_invite_path and return
     else
       flash[:error]  = "We couldn't set up that account, sorry.  Please try again, or contact an admin (link is above)."
       render :action => 'new'
