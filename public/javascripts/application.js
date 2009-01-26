a4682ec92e7d0988083af4f0a7c54cac2c29ea97
Collin VanDyck <gluedtomyseat@gmail.com>
2009-01-26 10:00:01 -0500

    Flash now lives at the top and is rolled up

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,11 +2,14 @@
 // This file is automatically included by javascript_include_tag :defaults
 
 Event.observe(window, "load", function() {
+  
+  // focus the form fields
   var field = $$('form input[type="text"]').first();
   if (field) {
     field.focus();
   }
-  
+
+  // color the tables
   $$('table').each(function(table) {
     var odd = true;
     table.select("tr").each(function(tr) {
@@ -17,5 +20,11 @@ Event.observe(window, "load", function() {
       }
     })
   })
+  
+  // rollup the flash
+  var flash = $("flash");
+  if (flash) {
+    new Effect.BlindUp("flash", {delay: 3});
+  }
 })
 
