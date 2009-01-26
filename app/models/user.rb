969d6dc2914a6642880e1352d6b8476ecf599c7b
Collin VanDyck <gluedtomyseat@gmail.com>
2009-01-26 11:28:42 -0500

    Removed the admin column from user

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,7 +30,7 @@ class User < ActiveRecord::Base
   # HACK HACK HACK -- how to do attr_accessible from here?
   # prevents a user from submitting a crafted form that bypasses activation
   # anything else you want your user to change should be added here.
-  attr_accessible :login, :email, :name, :password, :password_confirmation, :admin, :first_name, :last_name, :birthday, :gender, :occupation, :area_ids
+  attr_accessible :login, :email, :name, :password, :password_confirmation, :first_name, :last_name, :birthday, :gender, :occupation, :area_ids
 
   named_scope :ordered, lambda { |*sym| { :order => (sym.first || :email )}}
 
