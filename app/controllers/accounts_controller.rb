614910183df4e1ece188d93ed08bc6f8c18cfcae
Collin VanDyck <gluedtomyseat@gmail.com>
2009-01-25 20:19:55 -0500

    Edit account view

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,6 +1,15 @@
 class AccountsController < ApplicationController
   
   before_filter :set_nav
+  before_filter :login_required
+  
+  def update
+    if current_user.update_attributes params[:user]
+      flash[:notice] = "Account Updated"
+      redirect_to edit_account_path and return
+    end
+    render :action => "edit"
+  end
   
   private
   
