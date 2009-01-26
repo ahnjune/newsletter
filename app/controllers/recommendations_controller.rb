8aae0a5da5119a11b3a3d8d2ba847b2332b74876
Collin VanDyck <gluedtomyseat@gmail.com>
2009-01-26 14:31:13 -0500

    Recommendations has a home nav section now

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -1,9 +1,16 @@
 class RecommendationsController < ApplicationController
   
   before_filter :login_required
+  before_filter :set_nav
   
   def show
     @feed_items = current_user.generate_recommendations.paginate(:page => params[:page], :per_page => 15)
+  end
+  
+  private
+  
+  def set_nav
+    @nav = :recommendations
   end
   
 end
