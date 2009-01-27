eeef1b21c96ee24804fc02c44d1ed2abc73207aa
Collin VanDyck <gluedtomyseat@gmail.com>
2009-01-27 10:14:15 -0500

    List size of 25 for recommendations

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -4,7 +4,7 @@ class RecommendationsController < ApplicationController
   before_filter :set_nav
   
   def show
-    @recommendations = current_user.recommendations.ordered.paginate(:page => params[:page], :per_page => 15)
+    @recommendations = current_user.recommendations.ordered.paginate(:page => params[:page], :per_page => 25)
   end
   
   private
